1f43edcab1c16a5f4ede8a80e44d0aef9ac0232c
abdullahberat <pxenn@outlook.com>
2026-02-01 17:16:53 +0300

    prod ready

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const API_URL = import.meta.env.PROD ? '/api' : 'http://localhost:9561/api';
-const ADMIN_URL = import.meta.env.PROD ? '/admin/api' : 'http://localhost:9561/admin';
+const ADMIN_URL = import.meta.env.PROD ? '/admin' : 'http://localhost:9561/admin';
 
 // API instance oluştur
 const api = axios.create({
